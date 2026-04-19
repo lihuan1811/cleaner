583d76c67b2bb0aed662f5274b25b9eb771846d2
夏天。 <wonderfulsunshine@MacBook-Air.local>
2026-04-19 23:33:46 +0800

    更新软件卸载工具版本，更新工具包资源(LFS)

--- WinCleanerDlg.cpp
+++ WinCleanerDlg.cpp
@@ -595,28 +595,15 @@ void CWinCleanerDlg::OnBnClickedDiskDefrag()
 
 void CWinCleanerDlg::OnBnClickedSoftwareUninstall() {
 	LogMessage(_T("开始 [软件卸载]"));
-	CString fileName = _T("HiBitUninstaller.exe");
-	CString exePath = m_outDir + _T("2.系统维护工具\\3.软件卸载\\") + fileName;
-	if (_taccess(exePath, 0) != 0) {
-		exePath = m_outDir + _T("系统维护工具\\软件卸载\\") + fileName;
-	}
+	CString fileName = _T("Hibit Uninstaller 4.0.10.100 Multilingual.exe");
+	CString exePath = m_outDir + _T("2.系统维护工具\\3.软件卸载\\Hibit Uninstaller 4.0.10.100 Multilingual\\") + fileName;
+	CString exeDir = m_outDir + _T("2.系统维护工具\\3.软件卸载\\Hibit Uninstaller 4.0.10.100 Multilingual\\");
 	if (_taccess(exePath, 0) != 0) {
 		AfxMessageBox(_T("未找到软件卸载程序"));
 		return;
 	}
-	STARTUPINFO si = { sizeof(si) };
-	si.lpTitle = _T("软件卸载");
-	PROCESS_INFORMATION pi;
-	if (CreateProcess(exePath.GetBuffer(), NULL, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi))
-	{
-		LogMessage(_T("已启动软件卸载"));
-		CloseHandle(pi.hProcess);
-		CloseHandle(pi.hThread);
-	}
-	else
-	{
-		AfxMessageBox(_T("无法启动软件卸载程序"));
-	}
+	ShellExecute(NULL, _T("open"), exePath, NULL, exeDir, SW_SHOWNORMAL);
+	LogMessage(_T("已启动软件卸载"));
 }
 
 void CWinCleanerDlg::OnBnClickedSysSettingsOpt()
